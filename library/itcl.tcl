746a88bd283f9bd60e9f83893f0d5d925fac540f
davygrvy <davygrvy>
2001-04-14 21:35:54 +0000

    Patch ID #227860

--- library/itcl.tcl
+++ library/itcl.tcl
@@ -9,12 +9,16 @@
 #            mmclennan@lucent.com
 #            http://www.tcltk.com/itcl
 #
-#      RCS:  $Id: itcl.tcl,v 1.3 2000/03/28 19:12:42 csmith Exp $
+#      RCS:  $Id: itcl.tcl,v 1.4 2001/04/14 21:35:54 davygrvy Exp $
 # ----------------------------------------------------------------------
 #            Copyright (c) 1993-1998  Lucent Technologies, Inc.
 # ======================================================================
 # See the file "license.terms" for information on usage and
 # redistribution of this file, and for a DISCLAIMER OF ALL WARRANTIES.
+
+proc ::itcl::delete_helper { name args } {
+    ::itcl::delete object $name
+}
 
 # ----------------------------------------------------------------------
 #  USAGE:  local <className> <objName> ?<arg> <arg>...?
@@ -31,7 +35,7 @@ proc ::itcl::local {class name args} {
     uplevel [list set itcl-local-$ptr $ptr]
     set cmd [uplevel namespace which -command $ptr]
     uplevel [list trace variable itcl-local-$ptr u \
-        "itcl::delete object $cmd; list"]
+        "::itcl::delete_helper $cmd"]
     return $ptr
 }
 
