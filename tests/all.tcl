9b46cb7173b1f6bea5675aaa76b72bc97e61a515
davygrvy <davygrvy>
2004-02-12 18:09:50 +0000

    Modified test suite to use -loadfile and ::tcltest::loadTestedCommands in each test file.

--- tests/all.tcl
+++ tests/all.tcl
@@ -7,47 +7,11 @@
 # Copyright (c) 1998-2000 by Ajuba Solutions
 # All rights reserved.
 # 
-# RCS: @(#) $Id: all.tcl,v 1.3 2002/03/29 04:43:49 hobbs Exp $
+# RCS: @(#) $Id: all.tcl,v 1.4 2004/02/12 18:09:50 davygrvy Exp $
 
 package require tcltest
-namespace import -force ::tcltest::*
 
-set ::tcltest::testSingleFile false
-set ::tcltest::testsDirectory [file dirname [info script]]
+tcltest::testsDirectory [file dir [info script]]
+tcltest::runAllTests
 
-# We need to ensure that the testsDirectory is absolute
-::tcltest::normalizePath ::tcltest::testsDirectory
-
-puts stdout "Tests running in interp:       [info nameofexecutable]"
-puts stdout "Tests running with pwd:        [pwd]"
-puts stdout "Tests running in working dir:  $::tcltest::testsDirectory"
-if {[llength $::tcltest::skip] > 0} {
-    puts stdout "Skipping tests that match:            $::tcltest::skip"
-}
-if {[llength $::tcltest::match] > 0} {
-    puts stdout "Only running tests that match:        $::tcltest::match"
-}
-
-if {[llength $::tcltest::skipFiles] > 0} {
-    puts stdout "Skipping test files that match:       $::tcltest::skipFiles"
-}
-if {[llength $::tcltest::matchFiles] > 0} {
-    puts stdout "Only sourcing test files that match:  $::tcltest::matchFiles"
-}
-
-set timeCmd {clock format [clock seconds]}
-puts stdout "Tests began at [eval $timeCmd]"
-
-# source each of the specified tests
-foreach file [lsort [::tcltest::getMatchingFiles]] {
-    set tail [file tail $file]
-    puts stdout $tail
-    if {[catch {source $file} msg]} {
-	puts stdout $msg
-    }
-}
-
-# cleanup
-puts stdout "\nTests ended at [eval $timeCmd]"
-::tcltest::cleanupTests 1
-exit
+return
