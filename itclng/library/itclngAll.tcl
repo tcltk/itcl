0faece555030aea353ee35a0b5827f7d331aa6c4
wiede <wiede@users.sourceforge.net>
2008-01-12 23:55:32 +0000

    initial version

---
+++ itclng/library/itclngAll.tcl
@@ -0,0 +1,3 @@
+source [file join [file dirname [::info script]] itclngBase.tcl]
+source [file join [file dirname [::info script]] itclngParse.tcl]
+source [file join [file dirname [::info script]] itclngMember.tcl]
