038ddefbd1d5cbc67e1028cb3c9ab4b39254e165
wiede <wiede@users.sourceforge.net>
2008-11-14 12:10:41 +0000

    initial version

---
+++ itclWidget/tests/all.tcl
@@ -0,0 +1,17 @@
+# all.tcl --
+#
+# This file contains a top-level script to run all of the Tcl
+# tests.  Execute it by invoking "source all.test" when running tcltest
+# in this directory.
+#
+# Copyright (c) 1998-2000 by Ajuba Solutions
+# All rights reserved.
+# 
+# RCS: @(#) $Id: all.tcl,v 1.1.2.1 2008/11/14 12:10:41 wiede Exp $
+
+package require tcltest 2.1
+
+tcltest::testsDirectory [file dir [info script]]
+tcltest::runAllTests
+
+return
