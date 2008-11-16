66b9091198dc24beb4153233e974d25c030e121b
wiede <wiede@users.sourceforge.net>
2008-11-16 16:23:35 +0000

    implementation of widgetclass and hulltype commands

--- itclWidget/generic/itclWidgetParse.c
+++ itclWidget/generic/itclWidgetParse.c
@@ -41,7 +41,7 @@
  *
  *  Author: Arnulf Wiedemann
  *
- *     RCS:  $Id: itclWidgetParse.c,v 1.1.2.2 2008/10/25 19:41:49 wiede Exp $
+ *     RCS:  $Id: itclWidgetParse.c,v 1.1.2.3 2008/11/16 16:23:35 wiede Exp $
  * ========================================================================
  *           Copyright (c) 2007  Arnulf Wiedemann
  * ------------------------------------------------------------------------
@@ -53,19 +53,14 @@
 #include "itclWidgetInt.h"
 
 Tcl_ObjCmdProc Itcl_ClassComponentInstallCmd;
-Tcl_ObjCmdProc Itcl_ClassWidgetClassCmd;
-Tcl_ObjCmdProc Itcl_ClassHullTypeCmd;
 Tcl_ObjCmdProc Itcl_WidgetCmd;
 Tcl_ObjCmdProc Itcl_WidgetAdaptorCmd;
-Tcl_ObjCmdProc Itcl_TypeCmd;
 
 static const struct {
     const char *name;
     Tcl_ObjCmdProc *objProc;
 } parseCmds[] = {
     {"componentinstall", Itcl_ClassComponentInstallCmd},
-    {"hulltype", Itcl_ClassHullTypeCmd},
-    {"widgetclass", Itcl_ClassWidgetClassCmd},
     {NULL, NULL}
 };
 
@@ -100,10 +95,6 @@ Itcl_WidgetParseInit(
                 parseCmds[i].objProc, (ClientData) infoPtr, NULL);
         Tcl_DStringFree(&buffer);
     }
-
-    Tcl_CreateObjCommand(interp, "::itcl::type", Itcl_TypeCmd,
-        (ClientData)infoPtr, (Tcl_CmdDeleteProc*)NULL);
-    Itcl_PreserveData((ClientData)infoPtr);
 
     Tcl_CreateObjCommand(interp, "::itcl::widget", Itcl_WidgetCmd,
         (ClientData)infoPtr, (Tcl_CmdDeleteProc*)NULL);
@@ -151,115 +142,4 @@ Itcl_ClassComponentInstallCmd(
 iclsPtr = NULL;
     Tcl_AppendResult(interp, "componentinstall not yet implemented", NULL);
     return TCL_ERROR;
-}
-
-/*
- * ------------------------------------------------------------------------
- *  Itcl_ClassHullTypeCmd()
- *
- *  Invoked by Tcl during the parsing of a class definition whenever
- *  the "hulltype" command is invoked to define a hulltype 
- *  Handles the following syntax:
- *
- *      hulltype 
- *
- * ------------------------------------------------------------------------
- */
-int
-Itcl_ClassHullTypeCmd(
-    ClientData clientData,   /* info for all known objects */
-    Tcl_Interp *interp,      /* current interpreter */
-    int objc,                /* number of arguments */
-    Tcl_Obj *CONST objv[])   /* argument objects */
-{
-    int correctArg;
-
-    ItclShowArgs(1, "Itcl_ClassHullTypeCmd", objc, objv);
-    ItclObjectInfo *infoPtr = (ItclObjectInfo*)clientData;
-    ItclClass *iclsPtr = (ItclClass*)Itcl_PeekStack(&infoPtr->clsStack);
-    if (!(iclsPtr->flags & ITCL_WIDGET)) {
-        Tcl_AppendResult(interp, "\"", Tcl_GetString(iclsPtr->namePtr),
-	        " is no ::itcl::widget.", 
-		" Only an ::itcl::widget can have a hulltype", NULL);
-	return TCL_ERROR;
-    }
-    if (objc != 2) {
-        Tcl_WrongNumArgs(interp, 1, objv, " frame/toplevel");
-	return TCL_ERROR;
-    }
-    correctArg = 0;
-    if (strcmp(Tcl_GetString(objv[1]), "frame") == 0) {
-	iclsPtr->flags |= ITCL_WIDGET_FRAME;
-        correctArg = 1;
-    }
-    if (strcmp(Tcl_GetString(objv[1]), "labelframe") == 0) {
-	iclsPtr->flags |= ITCL_WIDGET_LABEL_FRAME;
-        correctArg = 1;
-    }
-    if (strcmp(Tcl_GetString(objv[1]), "toplevel") == 0) {
-	iclsPtr->flags |= ITCL_WIDGET_TOPLEVEL;
-        correctArg = 1;
-    }
-    if (strcmp(Tcl_GetString(objv[1]), "ttk::frame") == 0) {
-	iclsPtr->flags |= ITCL_WIDGET_TTK_FRAME;
-        correctArg = 1;
-    }
-    if (strcmp(Tcl_GetString(objv[1]), "ttk::labelframe") == 0) {
-	iclsPtr->flags |= ITCL_WIDGET_TTK_LABEL_FRAME;
-        correctArg = 1;
-    }
-    if (strcmp(Tcl_GetString(objv[1]), "ttk::toplevel") == 0) {
-	iclsPtr->flags |= ITCL_WIDGET_TTK_TOPLEVEL;
-        correctArg = 1;
-    }
-    if (!correctArg) {
-        Tcl_AppendResult(interp,
-	        "syntax: must be hulltype frame or toplevel",
-	        NULL);
-        return TCL_ERROR;
-    }
-    return TCL_OK;
-}
-
-/*
- * ------------------------------------------------------------------------
- *  Itcl_ClassWidgetClassCmd()
- *
- *  Invoked by Tcl during the parsing of a class definition whenever
- *  the "widgetclass" command is invoked to define a widgetclass 
- *  Handles the following syntax:
- *
- *      widgetclass 
- *
- * ------------------------------------------------------------------------
- */
-int
-Itcl_ClassWidgetClassCmd(
-    ClientData clientData,   /* info for all known objects */
-    Tcl_Interp *interp,      /* current interpreter */
-    int objc,                /* number of arguments */
-    Tcl_Obj *CONST objv[])   /* argument objects */
-{
-    ItclObjectInfo *infoPtr;
-    ItclClass *iclsPtr;
-
-    ItclShowArgs(1, "Itcl_ClassWidgetClassCmd", objc, objv);
-    infoPtr = (ItclObjectInfo*)clientData;
-    iclsPtr = (ItclClass*)Itcl_PeekStack(&infoPtr->clsStack);
-    if (objc != 2) {
-        Tcl_WrongNumArgs(interp, 1, objv, "<widgetclass>");
-        return TCL_ERROR;
-    }
-    if (!(iclsPtr->flags & ITCL_WIDGET)) {
-        Tcl_AppendResult(interp, "\"", Tcl_GetString(iclsPtr->namePtr),
-	        " is no ::itcl::widget.", 
-		" Only an ::itcl::widget can have a widgetclass", NULL);
-	return TCL_ERROR;
-    }
-    iclsPtr->widgetClassPtr = objv[1];
-    Tcl_IncrRefCount(iclsPtr->widgetClassPtr);
-    /* FIX ME !! */
-    /* maybe have to check for valid widget class name ?? */
-    Tcl_AppendResult(interp, "not yet completely implemented", NULL);
-    return TCL_OK;
 }
