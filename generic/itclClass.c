e3adb86c186b9ce0d817e16d4127ac4773eb0590
wiede <wiede@users.sourceforge.net>
2010-05-02 09:01:19 +0000

    fix for SF bug #2993540

--- generic/itclClass.c
+++ generic/itclClass.c
@@ -25,7 +25,7 @@
  *
  *  overhauled version author: Arnulf Wiedemann Copyright (c) 2007
  *
- *     RCS:  $Id: itclClass.c,v 1.1.2.55 2010/03/06 12:52:11 wiede Exp $
+ *     RCS:  $Id: itclClass.c,v 1.1.2.56 2010/05/02 09:01:19 wiede Exp $
  * ========================================================================
  *           Copyright (c) 1993-1998  Lucent Technologies, Inc.
  * ------------------------------------------------------------------------
@@ -146,12 +146,8 @@ void
 ItclDeleteClassMetadata(
     ClientData clientData)
 {
-    ItclClass *iclsPtr;
-
     /* do we need that at all ? */
-return;
-
-    iclsPtr = clientData;
+    return;
 }
 
 static int
@@ -606,26 +602,8 @@ ItclDeleteClassVariablesNamespace(
     Tcl_Interp *interp,
     ItclClass *iclsPtr)
 {
-    Tcl_DString buffer;
-    Tcl_Namespace *varNsPtr;
-
     /* TODO: why is this being skipped? */
-return;
-
-    if (iclsPtr->nsPtr == NULL) {
-        return;
-    }
-    /* free the classes's variables namespace and variables in it */
-    Tcl_DStringInit(&buffer);
-    Tcl_DStringAppend(&buffer, ITCL_VARIABLES_NAMESPACE, -1);
-    Tcl_DStringAppend(&buffer, iclsPtr->nsPtr->fullName, -1);
-    varNsPtr = Tcl_FindNamespace(interp, Tcl_DStringValue(&buffer),
-	    NULL, 0);
-    if (varNsPtr != NULL) {
-        Tcl_DeleteNamespace(varNsPtr);
-    }
-    Tcl_DStringFree(&buffer);
-    iclsPtr->nsPtr = NULL;
+    return;
 }
 
 int
