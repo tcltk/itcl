8d4aaf06f1fd36259034665c380587c664c60757
arnulf <arnulf>
2014-09-14 14:06:40 +0000

    added some debug output.

--- generic/itclTestRegisterC.c
+++ generic/itclTestRegisterC.c
@@ -45,10 +45,10 @@ cArgFunc(
     int result;
     ItclObjectInfo * infoPtr = NULL;
     ItclClass *iclsPtr = NULL;
+    ItclClass * classPtr;
     ItclObject * rioPtr = (ItclObject *)1;
     Tcl_Obj * objv[4];
     FOREACH_HASH_DECLS;
-    ItclClass * classPtr;
 
 //fprintf(stderr, "argc: %d\n", argc);
     if (argc != 4) {
@@ -97,6 +97,10 @@ cObjFunc(
     Tcl_Obj *const *objv)
 {
     Tcl_Namespace *nsPtr;
+    ItclObjectInfo * infoPtr = NULL;
+    ItclClass *iclsPtr = NULL;
+    ItclClass * classPtr;
+    FOREACH_HASH_DECLS;
     int i;
 
     ItclShowArgs(0, "cObjFunc called", objc, objv);
@@ -105,7 +109,16 @@ for(i = 0; i<objc;i++) {
     fprintf(stderr, "arg:%d:%s:\n", i, Tcl_GetString(objv[i]));
 }
     nsPtr = Tcl_GetCurrentNamespace(interp);
-fprintf(stderr, "IP:%p %p\n",interp, nsPtr);
+fprintf(stderr, "IP:%p %p %p !%s!\n",interp, clientData, nsPtr, nsPtr->fullName);
+    infoPtr = (ClientData)Tcl_GetAssocData(interp, ITCL_INTERP_DATA, NULL);
+    FOREACH_HASH_VALUE(classPtr,&infoPtr->nameClasses) {
+        if (strcmp(Tcl_GetString(objv[1]), Tcl_GetString(classPtr->fullNamePtr)) == 0 ||
+                strcmp(Tcl_GetString(objv[2]), Tcl_GetString(classPtr->fullNamePtr)) == 0) {
+           iclsPtr = classPtr;
+	   break;
+        }
+    }
+fprintf(stderr, "IP2:%p %p\n",interp, clientData, iclsPtr);
     return TCL_OK;
 }
 
@@ -115,6 +128,7 @@ RegisterDebugCFunctions(Tcl_Interp *interp)
 {
     int result;
 
+    /* args: interp, name, c-function, clientdata, deleteproc */
     result = Itcl_RegisterC(interp, "cArgFunc", cArgFunc, NULL, NULL);
     result = Itcl_RegisterObjC(interp, "cObjFunc", cObjFunc, NULL, NULL);
     if (result != 0) {
